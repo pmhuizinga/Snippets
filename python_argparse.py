97c841db37a7671e3f6ebdb073b7dd62570fb566
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2019-11-21 21:07:44 +0100

    Create python_argparse.py

---
+++ python_argparse.py
@@ -0,0 +1,20 @@
+# sample code for using arguments in case of calling python code from cmd.
+
+import argparse
+
+def main(args):
+
+    print(f"file location: {args.argument_name}")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Argument description here.')
+    parser.add_argument("--argument_name",
+                        type=Path,
+                        required=True,
+                        help="help text")
+
+    args = parser.parse_args()
+
+    parking = main(args)
