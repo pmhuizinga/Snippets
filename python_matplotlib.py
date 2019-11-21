f0dee35a3a253b56d53c28b38d8852e4d957dbdb
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2019-11-21 22:17:09 +0100

    Create python_matplotlib.py

---
+++ python_matplotlib.py
@@ -0,0 +1,43 @@
+# sample code for using matplotlib and seaborn
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+
+# basic line plot
+plt.figure(figsize=(20,10))
+plt.plot(df['x_values'], df['y_values'])
+plt.title('title')
+plt.xticks(rotation='vertical')
+plt.legend()
+plt.show()
+
+# basic bar chart
+plt.figure(figsize=(20,10))
+plt.bar(df['x_values'], df['y_values'])
+plt.show()
+
+# basic scatter plot
+# s = size
+# hue = distiction value
+fig, ax = plt.subplots(figsize=(20,20))
+ax = sns.scatterplot(x="x_column_name",
+                    y="y_column_name",
+                    data=df_wijk_coords[['x_column_name','y_column_name']], 
+                    s=1000, 
+                    hue=df['hue_column_name'])
+
+# basic 3D scatter plot
+fig = plt.figure(figsize=([20,20]))
+ax = fig.add_subplot(111, projection='3d')
+
+y = np.array(df['x_values'])
+x = np.array(df['y_values'])
+z = np.array(df['z_values'])
+
+ax.scatter(x, y, z)
+ax.set_xlabel('x label')
+ax.set_ylabel('y label')
+ax.set_zlabel('z label')
+plt.show()
+
