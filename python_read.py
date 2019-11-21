7cfe0f01aef656faf10fa4148158f1b01cebda2c
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2019-11-21 22:37:13 +0100

    Update python_read.py

--- python_read.py
+++ python_read.py
@@ -1,6 +1,5 @@
 # sample code for reading data from different sources
 
-
 # read csv
 # use low_memory in case of large files. Do check datatypes in that case.
 df_scan = pd.read_csv(filename, sep=';', decimal=",", header=None, low_memory=False) 
@@ -18,4 +17,5 @@ with urllib.request.urlopen(url) as file:
     
 # read html 
 df = pd.read_html('http://url.html',skiprows=4, header=0, decimal=',', thousands='.')
+
 
