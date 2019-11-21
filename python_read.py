415d2833d741f1c61987daf1df517caabc5eb095
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2019-11-21 21:29:53 +0100

    Update python_read.py

--- python_read.py
+++ python_read.py
@@ -1,1 +1,11 @@
 # sample code for reading data from different sources
+
+# read json data into pandas dataframe
+import pandas as pd
+import urllib.request
+import json
+
+with urllib.request.urlopen(url) as file:
+    a = json.loads(file.read())
+    df = pd.DataFrame.from_dict(a)
+    
