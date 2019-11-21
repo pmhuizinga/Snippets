a3508e6fede661189153865a391c3082d09549cb
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2019-11-21 22:28:00 +0100

    Update python_read.py

--- python_read.py
+++ python_read.py
@@ -1,5 +1,11 @@
 # sample code for reading data from different sources
 
+
+# read csv
+# use low_memory in case of large files. Do check datatypes in that case.
+df_scan = pd.read_csv(filename, sep=';', decimal=",", header=None, low_memory=False) 
+
+
 # read json data into pandas dataframe
 import pandas as pd
 import urllib.request
@@ -8,6 +14,7 @@ import json
 with urllib.request.urlopen(url) as file:
     a = json.loads(file.read())
     df = pd.DataFrame.from_dict(a)
+
     
 # read html 
 df = pd.read_html('http://url.html',skiprows=4, header=0, decimal=',', thousands='.')
