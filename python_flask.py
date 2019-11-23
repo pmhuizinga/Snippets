c36c84db118438232ac15963d90e8089c0388cc9
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2019-11-24 00:18:11 +0100

    Create python_flask.py

---
+++ python_flask.py
@@ -0,0 +1,12 @@
+# DIRECTORY STRUCTURE
+# main_folder/project.py : start file
+
+# main_folder/app/__init__.py : flask init file
+# main_folder/app/routes.py : contains routing
+# main_folder/app/models.py : contains database stuff (sqlalchemy)
+# main_folder/app/forms.py :  contains form stuff
+
+# main_folder/app/templates/.. : contains html files
+
+RUN FLASK, in terminal run: set FLASK_APP=project.py
+
