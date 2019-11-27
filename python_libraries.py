c3be1b43d3cc266261053c2c2d39f1d6355d92e5
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2019-11-27 11:11:17 +0100

    Create python_libraries.py

---
+++ python_libraries.py
@@ -0,0 +1,14 @@
+import pandas as pd						  data handling/wrangling
+imort numpy as np						    linear algebra, etc				
+imprt os						            file/folder looping and more
+from pathlib import Path
+import sqlite3 					        database
+import sqlalchemy					      database connections
+import logging					        logging
+from matplotlib	import pyplot		charts, plotting
+import configparser					    config
+import re						            regular expressions
+import datetime					        dates
+import networkx					        graph database
+import seaborn as sms					  charts, plotting
+import sklearn					        machine learning
