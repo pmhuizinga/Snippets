4d56790493b5b8b21dc902b62bb0eac9e3efe673
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2019-11-27 11:24:48 +0100

    Create python_networkx.py

---
+++ python_networkx.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# create sample data
+node_list = ['A', 'B', 'C', 'D']
+source = ['A', 'B', 'C', 'D']
+target = ['B', 'C', 'D', 'A']
+df = pd.DataFrame({'source': source, 'target': target})
+
+# create graph database
+G = nx.Graph()
+
+# add nodes
+G.add_nodes_from(node_list, type="test")
+
+# add edges
+edges = []
+for index, row in df.iterrows():
+    print(row)
+    edges.append((row['source'], row['target']))
+
+G.add_edges_from(edges)
+
+# draw graph
+plt.figure(3, figsize=(12, 12))
+nx.draw(G)
+plt.show()
