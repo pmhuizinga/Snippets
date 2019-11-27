ca32f47eb7f90c4730325a219b6fac5db4c3d0f8
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2019-11-27 13:30:36 +0100

    Update python_networkx.py

--- python_networkx.py
+++ python_networkx.py
@@ -26,3 +26,7 @@ G.add_edges_from(edges)
 plt.figure(3, figsize=(12, 12))
 nx.draw(G)
 plt.show()
+
+# alternative way to 'populate' G using a dataframe:
+G = nx.from_pandas_dataframe(df=bd_sub, source='Source', target='Target', edge_attr='gender', 
+                            create_using=nx.Graph())
