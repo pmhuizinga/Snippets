0dad0f713238cc066fedda1fbd8d37b068bc406a
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2020-01-28 20:35:58 +0100

    Update python_matplotlib.py

--- python_matplotlib.py
+++ python_matplotlib.py
@@ -9,6 +9,8 @@ import numpy as np
 fig.patch.set_facecolor('xkcd:gray') # graph background
 ax1.set_facecolor('xkcd:salmon') # axis part background
 
+#------------------------------------------------------------------
+
 # line plot
 plt.figure(figsize=(20,10))
 plt.plot(df['x_values'], df['y_values'])
@@ -25,13 +27,14 @@ plt.axvline(np.quantile(df.index.values, .50), color='orange', label='label') # 
 plt.savefig('filename.png')
 plt.show()
 
+#------------------------------------------------------------------
 
 # basic bar chart
 plt.figure(figsize=(20,10))
 plt.bar(df['x_values'], df['y_values'])
 plt.show()
 
-
+#------------------------------------------------------------------
 # basic scatter plot
 # s = size
 # hue = distiction value
@@ -62,3 +65,19 @@ ax.set_ylabel('y label')
 ax.set_zlabel('z label')
 plt.show()
 
+#------------------------------------------------------------------
+# create subplots with 3 columns and a variable number of rows
+nr_or_rows = math.ceil(float(len([list]) / 3.0))
+fig = plt.figure()
+fig, ax = plt.subplots(nr_or_rows, 3, sharey=True, figsize=(20,10))
+
+#plot positions for each entity in list
+for nr, entity in enumerate([list]):
+    row = math.ceil((nr + 1) / 3) - 1
+    column = round(((((nr + 1) / 3)) - row) * 3) -1
+    df = df_pos_total[df_pos_total.entity_name == entity]
+    x = df.instrument_name
+    y = df.value
+
+    ax[row][column].bar(x,y)
+    ax[row][column].set_title(entity.upper())
