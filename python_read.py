e26d8e85f0ee0c60c8d7f512db4c119c50d100d9
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2020-03-01 18:04:04 +0100

    Update python_read.py

--- python_read.py
+++ python_read.py
@@ -34,7 +34,7 @@ def read_data(file, sheet):
     return df
 
 # read SQL
-engine="mssql://UKDBCSDEVCLD001\CLD01DEV/Markit_EDM_TEST_v95?trusted_connection=yes;driver=SQL+Server"
+engine="mssql://<server>\<server2>/<database name>?trusted_connection=yes;driver=SQL+Server"
 qry = "select column from table"
 df = pd.read_sql_query(qry, con=engine, parse_dates=None)
 
