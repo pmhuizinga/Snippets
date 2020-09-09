f38def38098c918e2b46d84776c5213838e2fc48
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2020-09-09 11:16:29 +0200

    Create sql_table_variable.sql

---
+++ sql_table_variable.sql
@@ -0,0 +1,14 @@
+-- create temporary table
+
+DECLARE @tablename table
+	(
+	column1 nVARCHAR(20)
+	,column2 DATETIME
+	,column3 INT
+	,column4 decimal(38,18)
+	)
+INSERT INTO @tablename
+SELECT	column1
+		,column2
+		,column3
+		,column4
