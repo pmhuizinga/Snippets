a86ab60f65a52737af49a00394863d2d288dbd48
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2020-09-10 09:57:22 +0200

    Update sql_general.sql

--- sql_general.sql
+++ sql_general.sql
@@ -1,14 +1,14 @@
 -- LAG function: get the previous value in the current record
 select	column1
-  		  ,column2
-	  		,lag(column3,1) over (order by column1, columns2) as PreviousValue
-From		table
+  	,column2
+	,lag(column3,1) over (order by column1, columns2) as PreviousValue
+From	table
 
 -- ISNULL function: in case of null replace with alternative value
 select	ISNULL(column, alternative column)
-From		table
+From	table
 
 -- COALESCE function: Return the first non-null value in a list
 select	ISNULL(column1, column2, column3)
-From		table
+From	table
 
