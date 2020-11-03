9d7d5f14d2e351a803d0f1ac8108851f5bb87c4c
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2020-11-03 21:44:04 +0100

    Update d3_basics.js

--- d3_basics.js
+++ d3_basics.js
@@ -1,14 +1,14 @@
-BASIC SETUP
-set static variables (width, height, svg, g, ax scaling, labels, axis
-get data, include interval function, include update function
-update function: include transition, element join, remove old elements, enter new elements
+//BASIC SETUP
+//set static variables (width, height, svg, g, ax scaling, labels, axis
+//get data, include interval function, include update function
+//update function: include transition, element join, remove old elements, enter new elements
 
 
-DEBUGGING
+//DEBUGGING
 console.log("Text value:", variable)
 
 
-ERRORHANDLING
+//ERRORHANDLING
 d3.json("data/ages.json")
 	.then(data => {
 	<procedure>
@@ -17,7 +17,7 @@ d3.json("data/ages.json")
 })
 
 
-IF -> THEN -> ELSE
+//IF -> THEN -> ELSE
 if function
 			if (d.name === "Tony") {
 				return "blue"
@@ -27,7 +27,7 @@ if function
 			}
       
       
-SCALING
+//SCALING
 const x = d3.scalelinear().domain([1,2]).range([10, 20])
 const x = d3.scaleLog().domain([1,200]).range([0, 10]).base(10)
 const x = d3.scaleTime().domain([new Date(2000, 0, 1]), new Date(2001, 0, 1)]).range([0, 400])
@@ -36,17 +36,17 @@ const x = d3.scaleBand().domain(["a", "b", "c"]).range([0, 200]).paddingInner(0.
 x.bandwith()
 
 
-MIN, MAX, MAP
+//MIN, MAX, MAP
 const min = d3.min(data, d => d.value)
 const extent = d3.extent(data, d => d.value) (geeft bereik)
 const map = data.map(d => d.<text value column>) (zet text op volgorde)
 
 
-INTERVAL
+//INTERVAL
 d3.interval(()=> {console.log("testing"}, 1000)
 
 
-BOOTSTRAP GRID SYSTEM
+//BOOTSTRAP GRID SYSTEM
 <div class='row'>
 	<div class='col-xs-12> 12 kolommen voor een klein scherm</div>
 </div>
@@ -58,12 +58,13 @@ er zijn: col-xs-, col-sm-, col-md-, col-lg-
 </div>
 
 
-D3.JS UPDATE PATTERN
+//D3.JS UPDATE PATTERN
 entity.exit().remove()   	  	removes all from screen d3
 entity.attr(actions)			updates
 entity.enter().append(actions) 	add new
 
 
+// CODE HINTS
 const value = flag ? "profit" : "revenue" (if flag == false then revenue, else profit)
 
 
