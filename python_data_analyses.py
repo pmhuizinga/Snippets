38302586319e57b8b6f130d3db206c3c58b02c8b
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2020-11-03 21:36:03 +0100

    Update python_data_analyses.py

--- python_data_analyses.py
+++ python_data_analyses.py
@@ -6,4 +6,7 @@ pd.options.display.max_colwidth = 10000
 # use part of dataframe
 df = df.sample(frac=0.1)
 
+# group by
+df[['column1','column2']].groupby(['column1']).mean()
+df[['column1','column2','column3','column4']].groupby(['column1','column2']).agg(['mean','count']).sort_values(by=[('column3','mean')])
 
