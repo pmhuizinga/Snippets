4e27f2d2b023651797f92a259db5a2c4d60e9390
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2021-02-05 09:07:45 +0100

    Update python_datahandling.py

--- python_datahandling.py
+++ python_datahandling.py
@@ -5,6 +5,8 @@
 df['datecolumn'] = pd.to_datetime(df['datecolumn'], format="%Y-%m-%d %H:%M:%S.0000000")
 df['datecolumn'] = df['datecolumn'].dt.date
 df['datecolumn'] = pd.to_datetime(df['datecolumn'])
+# or
+df['datecolumn'] = df['datecolumn'].astype("datetime64")
 # convert to time
 df['timecolumn'] = pd.to_datetime(df['timecolumn'],format= '%H:%M' ).dt.time
 # extract hour value in column
