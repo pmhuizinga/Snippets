12316005c6b02262799b8e7e9a49a3bcff424898
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2021-02-05 09:14:43 +0100

    Update python_data_analyses.py

--- python_data_analyses.py
+++ python_data_analyses.py
@@ -4,9 +4,20 @@ import pandas as pd
 pd.options.display.max_colwidth = 10000
 
 # use part of dataframe
-df = df.sample(frac=0.1)
+df.sample(frac=0.1)
+# or
+df.sample(n=200)
 
 # group by
 df[['column1','column2']].groupby(['column1']).mean()
 df[['column1','column2','column3','column4']].groupby(['column1','column2']).agg(['mean','count']).sort_values(by=[('column3','mean')])
 
+# where not + isin
+df[~df['column1'].isin([1,2,3])]
+
+# value counts with normalization
+df['column'].value_counts(normalize=True)
+
+# unique
+df['column'].unique() # items
+df['column'].nunique() # number of items
