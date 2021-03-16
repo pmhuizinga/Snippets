3dce4d1fd2fd87ddf2ca6690d8db81e478d4587d
pmhuizinga <32456342+pmhuizinga@users.noreply.github.com>
2021-03-16 16:32:22 +0100

    Update python_data_analyses.py

--- python_data_analyses.py
+++ python_data_analyses.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 # adjust columns on dataframe output
 pd.options.display.max_colwidth = 10000
+# or
+pd.set_option('display.max_columns', None)
 
 # use part of dataframe
 df.sample(frac=0.1)
